44919ae20ae43df8800e7ddbe2ae087b822592b7
Tosu <167279662+about-tosu@users.noreply.github.com>
2024-08-26 03:03:46 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,10 +5,10 @@ SUDO = "5171347305"
 class Config:
     API_ID = int(getenv("API_ID", "14055090"))
     API_HASH = getenv("API_HASH", "a46f7b439d0afa45b7a69fc450f754e9")
-    BOT_TOKEN = getenv("BOT_TOKEN", "5616982320:AAFk0OhEkbFuUMrSSs6dfLdBYhxUI0c9Pt4")
-    FSUB = getenv("FSUB", "EmoBotDevolopers")
-    CHID = int(getenv("CHID", "-1001551504918"))
+    BOT_TOKEN = getenv("BOT_TOKEN", "7043502393:AAEdH1AegBbo9KIA91jWcie7KRE9gXXG5kw")
+    FSUB = getenv("FSUB", "about_tosuu")
+    CHID = int(getenv("CHID", "-1002023182491"))
     SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://rishbro:rishbro@cluster0.eiqoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://botmaker9675208:botmaker9675208@cluster0.sc9mq8b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
 cfg = Config()
